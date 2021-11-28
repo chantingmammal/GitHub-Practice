d2df8a0cc3a45ad0128de574cae9e0258f1ca42a
Nigerian Prince <730338@pdsb.net>
2021-11-28 15:39:00 -0500

    Theo added a funny comment

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // This is a test
         System.out.println("Hello, World!"); //Funny comment 
+// even funnyer comment
     }
 }
 
