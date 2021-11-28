22bcef2efdf17c69f4c0784d67444cab693c0176
tech support <nextmail2007@gmail.com>
2021-11-28 18:32:02 -0500

    funny number options

--- App.java
+++ App.java
@@ -9,30 +9,40 @@ public class App {
         String input1 = questions.nextLine();
 
         System.out.println("Question 2:What happened at tiananmen square?");
+        System.out.println("options are  : Nothing , Protesting , tank man ");
         String input2 = questions.nextLine();
 
         System.out.println("Question 3: Who is our leader?");
+        System.out.println("options are : Daniel zhou , Donald Trump ,Xi Jinping");
         String input3 = questions.nextLine();
 
         System.out.println("Question 4:is capitalism good?");
+        System.out.println("options are  : Yes, No");
         String input4 = questions.nextLine();
         
         System.out.println("Question 5: U.S.A or CHINA?");
+        System.out.println("Options are : U.S.A , CHINA");
         String input5 = questions.nextLine();
 
         System.out.println("Question 6: what is the best number of kids to have?");
+        System.out.println("Options are : 1, 4 , 69 , 420");
         String input6 = questions.nextLine();
 
         System.out.println("Question 7:Is china a capitalist or communist? ");
+        System.out.println("option are : capitalist or communist");
         String input7 = questions.nextLine();
 
         System.out.println("Question 8 :Who is tank man?");
+        System.out.println("options are : what are you talking about? , idk , wut , a protester");
         String input8 = questions.nextLine();
+        
 
         System.out.println("Question 9: What is better than china?");
+        System.out.println("options are : nothing , women , money , jeff bezos");
         String input9 = questions.nextLine();
 
         System.out.println("Question 10: Who is numba won enemy?");
+        System.out.println("simps, youtube ads , donald trump , johnny depp");
         String input10 = questions.nextLine(); //test fnny
     }
 }
