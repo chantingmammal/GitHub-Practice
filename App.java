f61b2692dfb22e19529c7d9cf731bc1d92f84298
ezra <147104973+chantingmammal@users.noreply.github.com>
2024-02-09 16:17:36 -0500

    Update App.java

--- App.java
+++ App.java
@@ -4,37 +4,37 @@ public class App {
     public static void main(String[] args) throws Exception {
         boolean retry;
 
-        System.out.println("Welcome to the social credit score test"); 
+        System.out.println("Welcome to the Beatles test"); 
         System.out.println("there will be 10 questions, are you ready?");
 
         do {
             Scanner questions = new Scanner(System.in);
             String[] inputs = new String[10];
             
-            System.out.println("Question 1: True or False, Taiwan a country?");
+            System.out.println("Question 1: True or False, Is John Lennon Alive?");
             inputs[0] = questions.nextLine();
             System.out.println(inputs[0]);
 
-            System.out.println("Question 2:What happened at tiananmen square?");
-            System.out.println("options are  : 1)Nothing   2)Protesting 3)tank man ");
+            System.out.println("Question 2:Who wrote "Voice Piece for Soprano"?");
+            System.out.println("options are  : 1)Yoko Ono 2)Donald Trump 3)Nathan Amaya ");
             System.out.println("Please type 1, 2 or 3");
             inputs[1] = questions.nextLine();
 
-            System.out.println("Question 3: Who is our leader?");
-            System.out.println("options are : 1)Daniel zhou 2)Donald Trump 3)Xi Jinping");
+            System.out.println("Question 3: In this song, it states Hard Times are Over for a While, what is the name?");
+            System.out.println("options are : 1)Kiss Kiss Kiss 2)Mundian to Bach Ke 3)Hard Times are Over");
             System.out.println("Please type 1, 2 or 3");
             inputs[2] = questions.nextLine();
 
-            System.out.println("Question 4:is capitalism good?");
+            System.out.println("Question 4:Did Yoko Ono breakup the Beatles?");
             System.out.println("options are  : 1)Yes  2)No");
             System.out.println("Please type 1 or 2");
             inputs[3] = questions.nextLine();
             
-            System.out.println("Question 5: U.S.A or CHINA?");
-            System.out.println("Options are : 1)U.S.A 2)CHINA");
+            System.out.println("Question 5: Yoko Ono or Linda McCartney?");
+            System.out.println("Options are : 1)ONO 2)MCCARTNEY");
             System.out.println("Please type 1 or 2");
             inputs[4] = questions.nextLine();
-
+// Rest of Questions need to be finished
             System.out.println("Question 6: what is the best number of kids to have?");
             System.out.println("Options are : 1)1 2)4  3)69 4)420");
             System.out.println("Please type 1, 2, 3 or 4");
